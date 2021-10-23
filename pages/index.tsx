444d70e650b8a6995d4050015e37db22f9b5f86e
Muhamad Riyan <riyan@skyshi.com>
2021-10-24 05:49:04 +0700

    setup data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,36 @@
 import Layout from 'components/Layout'
 import List from 'components/List'
 import Modal from 'components/Modal'
+import { Absent } from 'interfaces'
+import { NextPageContext } from 'next'
+const host = !!process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000'
 
-const IndexPage = () => (
-    <Layout title="StaffAny Assesment">
-        <h1>StaffAny Assessment</h1>
-        <Modal />
-        <List />
-    </Layout>
-)
+export async function getServerSideProps(context: NextPageContext) {
+    const { id, start, coupon } = context.query
+    const absentsResult = await fetch(`${host}/api/absents`)
+    const absents = await absentsResult.json()
+    return {
+        props: {
+            absents,
+        }, // will be passed to the page component as props
+    }
+}
+type Props = {
+    absents?: Absent[]
+}
+
+const IndexPage = ({ absents }: Props) => {
+    console.log(absents)
+
+    return (
+        <Layout title="StaffAny Assesment">
+            <h1>StaffAny Assessment</h1>
+            <Modal />
+            <List />
+        </Layout>
+    )
+}
 
 export default IndexPage
